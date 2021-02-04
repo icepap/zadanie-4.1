e641c50b5ed09be841bfe7c0d286e48355465a6e
Solo <email@example.ru>
2021-02-04 21:50:55 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace zadanie_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string A;
+              string B;
+              string C = A!= B;
+            
+
+            int A;
+            int B;
+            double X;
+            double Y;
+            bool C = (A < B) | (X > Y);
+            
+
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);
+            Console.ReadKey();
+            */
+
+
+
+            Console.WriteLine("Введите свой любимвый цвет на английском и с маленькой буквы");
+            var color = Console.ReadLine();
+
+            if (color == "red")
+                {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+
+                }
+            else if (color == "green")
+                {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                }
+            else
+                {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                 }
+
+            Console.ReadKey();
+
+        }
+    }
+}
